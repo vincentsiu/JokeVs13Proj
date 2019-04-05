95cd9a277f75bf243d62b071d4c107e94380ac0e
Vincent Siu <vincent.siu@flir.com>
2019-04-05 09:40:48 -0700

    first commit

---
+++ JokeVS2013Proj/JokeVS2013Proj.cpp
@@ -0,0 +1,13 @@
+// JokeVS2013Proj.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    std::cout << "Hello" << std::endl;
+	return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# JokeVs13Proj
